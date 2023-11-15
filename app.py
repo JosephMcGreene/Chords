5bda761a80af33ec8fbaa9795284457f02ac309b
Joseph McGreene <josephorianmcgreene@gmail.com>
2023-11-15 07:41:07 -0800

    Add user input for key accidental

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 notes = ["C", "D", "E", "F", "G", "A", "B"]
 
 def key_tonic():
-	tonic = input("What Key is the music in? ")
+	tonic = input("What key is the music in? ")
 	
 	if not notes.__contains__(tonic.upper()):
-		tonic = input("What Key is the music in? ")
+		tonic = input("What key is the music in (A-G please)? ")
 	
 	return tonic.upper()
+
+def key_accidental():
+	accidental = input("sharp (#), flat (b), or natural (nat)? ")
+
+	if accidental != "#" and accidental != "b" and accidental != "nat":
+		accidental = input('"#", "b", or "nat": ')
+
+	return accidental
 
 
 def key_quality():
 	quality = input("Major or minor? ")
 
 	if quality != "Major" and quality != "minor":
-		quality = input("Major or minor? ")
+		quality = input('"Major" or "minor"? ')
 
 	return quality
 
-print(f"The key of the music is {key_tonic()} {key_quality()}")
+print(f"The key of the music is {key_tonic()}{key_accidental()} {key_quality()}")
